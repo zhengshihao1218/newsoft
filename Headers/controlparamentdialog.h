c2ed38c9da84f129d277e815005db5489b1dfe4c
zheng_s1 <zhengshihao1218@163.com>
2025-12-22 10:04:41 +0800

    [new soft] control parament dialog get PLC data and show

--- Headers/controlparamentdialog.h
+++ Headers/controlparamentdialog.h
@@ -20,10 +20,17 @@ public:
 private slots:
     void on_tabWidget_2_currentChanged(int index);
 
+    void on_tabWidget_currentChanged(int index);
+
+    void on_doubleSpinBox_185_valueChanged(double arg1);
+
 private:
     Ui::ControlParamentDialog *ui;
     QTimer* tmUpdate;
+    // 实时获取轴1实际数据
     void updateDBValue();
+    // 单独获取并显示工位1位置页面的信息
+    void update_tab1_pos_value();
 };
 
 #endif // CONTROLPARAMENTDIALOG_H
